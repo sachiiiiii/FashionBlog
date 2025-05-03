d0057f000aa2d31a9d128121b9f2127b76b4316d
Osi <osachi752@gmail.com>
2025-05-02 21:35:42 -0400

    modified images' src attributes

--- ReactRefactor/src/components/Article.jsx
+++ ReactRefactor/src/components/Article.jsx
@@ -8,7 +8,7 @@ export default function Article() {
                 <article>
                     <time datetime="2020-11-12">11/12/20</time>
                     <h2>On the Street in Brooklyn</h2>
-                    <img src="blog-image-1.jpg" alt="A woman in a red dress stands by a blue wall in an industrial area of Brooklyn." />
+                    <img src="./images/blog-image-1.jpg" alt="A woman in a red dress stands by a blue wall in an industrial area of Brooklyn." />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit voluptatum, officiis animi possimus unde commodi enim nihil velit eaque impedit accusamus deleniti inventore eligendi magni veniam et sapiente repudiandae nemo!
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. A quas, aut perspiciatis at commodi omnis debitis officiis minima pariatur, quidem reiciendis natus odio dolorem atque voluptates nulla, ad assumenda hic!
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat dignissimos, nobis numquam iure possimus maiores error hic dolores porro, assumenda earum magni mollitia. Aperiam ipsa dolorem fugit rem officiis mollitia!
@@ -21,7 +21,7 @@ export default function Article() {
                 <article>
                     <time datetime="2020-11-12">11/12/20</time>
                     <h2>Vintage In Vogue</h2>
-                    <img src="blog-image-2.jpg" alt="Three people in vintage-inspired clothing at night with city lights in the background." />
+                    <img src="./images/blog-image-2.jpg" alt="Three people in vintage-inspired clothing at night with city lights in the background." />
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque saepe sapiente aut, aperiam, inventore odio incidunt expedita dolorem iure laboriosam, et pariatur maiores? Minus nam quisquam labore nostrum aliquid facere.
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, nostrum impedit ipsum non labore odit fugiat facilis veniam reprehenderit neque, quisquam illo, consequatur debitis sunt repudiandae velit praesentium possimus nisi!
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium beatae non quia dignissimos deserunt consectetur molestiae. Ab cumque blanditiis sint magni, tenetur facilis velit. Rem numquam voluptate obcaecati impedit accusamus?
